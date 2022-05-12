69dac3dc47a68904e3e3f3d73cf18c14de6a1c6f
NiekvAlphen <409046@student.fontys.nl>
2022-05-12 16:23:21 +0200

    change config.py

---
+++ songService/app/config.py
@@ -0,0 +1,41 @@
+# /instance/config.py
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config(object):
+    """Parent configuration class."""
+    DEBUG = False
+    CSRF_ENABLED = True
+    SECRET = os.getenv('SECRET')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    STATIC_FOLDER = f"{os.getenv('APP_FOLDER')}/static"
+
+class DevelopmentConfig(object):
+    """Configurations for development."""
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@song-db:5432/flask_api'
+    DEBUG = True
+
+class TestingConfig(object):
+    """Configurations for Testing, with a seperate test database."""
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost/test_db'
+    DEBUG = True
+
+class StagingConfig(object):
+    """Configurations for Staging."""
+    DEBUG = True
+
+class ProductionConfig(object):
+    """Configurations for Production."""
+    DEBUG = False
+    TESTING = False
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'staging': StagingConfig,
+    'production': ProductionConfig,
+}
